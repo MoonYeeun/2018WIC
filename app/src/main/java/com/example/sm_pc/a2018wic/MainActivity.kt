71e02d0e96d74776c1c9bfcb3d7dfa639a325964
MoonYeeun <aams82@hanmail.net>
2018-11-23 10:59:47 +0900

    WIC

---
+++ app/src/main/java/com/example/sm_pc/a2018wic/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sm_pc.a2018wic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
